d702136efb6df00a3510e30fc120521fd6258bab
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-11-11 02:12:11 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            String response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1); 
+
+                while(guess != number){
+
+                    Console.WriteLine("Guess the number between " + " " + min + " - " + max + ":");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if(guess > number)
+                    {
+                        Console.WriteLine(guess + "is high");
+                    }else if (guess < number){
+                        Console.WriteLine(guess + " is too low");
+                    }
+                    guesses++;
+
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("YOU WIN");
+                Console.WriteLine("Guesses: " + guesses);
+                Console.WriteLine("Would you like to play again? Y/N");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if(response == "Y")
+                {
+                    playAgain = true; 
+                }else if(response == "N")
+                {
+                    playAgain = false;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
